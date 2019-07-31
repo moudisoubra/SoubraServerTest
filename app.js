cc325c468d4e5852719b08da2277e044243fb9ed
moudisoubra <moudisoubra001@gmail.com>
2019-07-31 17:09:35 +0400

    Server Setup With Array

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 'use strict';
-
+var restify = require("restify");
+var io = require("fs");
 console.log('Hello world');
+var server = restify.createServer();
+var leaderBoard = [];
+server.get("/leaderBoard", function (req, res, next) {
+    res.send(JSON.stringify({ "leaderBoard":leaderBoard }));
+    next();
+});
+server.get("/AddleaderBoard/:playerID/:score", function (req, res, next) {
+    var obj = {};
+    obj.id = req.params.playerID;
+    obj.score = req.params.score;
+    leaderBoard.push(obj);
+    res.send(JSON.stringify(leaderBoard));
+    next();
+});
+server.listen(3000, function () {
+
+});
+setInterval(1000, function () {
+
+
+});
