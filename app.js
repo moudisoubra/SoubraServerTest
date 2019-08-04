6125740a7daf8d9d7af5f51611fcb49857ca25f9
moudisoubra <moudisoubra001@gmail.com>
2019-08-04 16:47:36 +0400

    Idk what I did honestly

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 'use strict';
 var restify = require("restify");
 var io = require("fs");
-console.log('Hello world');
+console.log('Hello Soubra, Server activated');
 var server = restify.createServer();
 var playerProfile = [];
-server.get("/leaderBoard", function (req, res, next) {
-    res.send(JSON.stringify({playerProfile}));
-    next();
-});
+
 server.get("/AddPlayerProfile/:playerID/:score/:aesthetic", function (req, res, next) {
+    console.log('Add player');
     var obj = {};
     obj.id = req.params.playerID;
     obj.score = req.params.score;
@@ -19,16 +17,18 @@ server.get("/AddPlayerProfile/:playerID/:score/:aesthetic", function (req, res, 
 });
 
 server.get("/Print", function (req, res, next) {
+console.log('print');
     res.send({playerProfile});
     next();
 });
 
 server.get("/DeleteArray", function (req, res, next) {
+console.log('print');
     playerProfile.length = 0;
     res.send({ playerProfile });
     next();
 });
 
 server.listen(process.env.PORT || 3000, function () {
-
+    console.log(process.env.PORT);
 });
