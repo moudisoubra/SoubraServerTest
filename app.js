b41b9d65e77aab1c7e8ace44ff93788fae86dd95
moudisoubra <moudisoubra001@gmail.com>
2019-08-04 18:00:49 +0400

    Added saving

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 'use strict';
 var restify = require("restify");
-var io = require("fs");
+var fs = require("fs");
 console.log('Hello Soubra, Server activated');
 var server = restify.createServer();
 var playerProfile = [];
 
 server.get("/AddPlayerProfile/:playerID/:score/:aesthetic", function (req, res, next) {
+
     console.log('Add player');
     var obj = {};
     obj.id = req.params.playerID;
@@ -14,20 +15,47 @@ server.get("/AddPlayerProfile/:playerID/:score/:aesthetic", function (req, res, 
     playerProfile.push(obj);
     res.send({playerProfile});
     next();
+
 });
 
 server.get("/Print", function (req, res, next) {
-console.log('print');
+
+    console.log('print');
     res.send({playerProfile});
     next();
+
 });
 
 server.get("/DeleteArray", function (req, res, next) {
-console.log('print');
+
+    console.log('print');
     playerProfile.length = 0;
     res.send({ playerProfile });
     next();
+
 });
+
+server.get("/ReadFile", function (req, res, next) {
+
+    fs.readFile("temp.txt", function (err, buf) {
+        res.send(buf.toString());
+    });
+});
+
+function SavingToFile()
+{
+    var data = "Save this";
+
+    fs.writeFile("temp.txt", data, (err) => {
+        if (err) console.log(err);
+        console.log("Successfully Written to File.");
+
+    });
+
+}
+
+setInterval(SavingToFile, 3000);
+    
 
 server.listen(process.env.PORT || 3000, function () {
     console.log(process.env.PORT);
